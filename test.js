605a0bb5ebc43a1505181c11dd3d7b838f350e0d
MinghuiJia <minghui_jia@whu.edu.cn>
2023-01-04 23:05:38 +0800

    已学完第四章强制类型转换

---
+++ test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: jiaminghui
+ * @Date: 2023-01-04 22:31:49
+ * @LastEditTime: 2023-01-04 22:31:58
+ * @LastEditors: jiaminghui
+ * @FilePath: \JavaScript_Learn\test.js
+ * @Description: 
+ */
+console.log([null].toString() === "")
