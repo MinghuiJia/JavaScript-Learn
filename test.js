250ef4cf633d445630a0568973bceda55ed6ade6
MinghuiJia <minghui_jia@whu.edu.cn>
2023-01-10 22:20:35 +0800

    修订完类型文档中类型和值部分的书写错误

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
 /*
  * @Author: jiaminghui
  * @Date: 2023-01-04 22:31:49
- * @LastEditTime: 2023-01-04 22:31:58
+ * @LastEditTime: 2023-01-10 21:19:27
  * @LastEditors: jiaminghui
  * @FilePath: \JavaScript_Learn\test.js
  * @Description: 
  */
 console.log([null].toString() === "")
+
+var arr = [1,2,3,4];
+console.log(arr);
+delete arr[1];
+delete arr[2];
+console.log(arr);
+console.log(arr.length);
+for (var i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
+var bbbb = [];
+bbbb[0] = 5;
+bbbb[2] = 10;
+console.log(bbbb);
